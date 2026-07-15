b66338342f5a2c0cb4c7c781b2fa565abcfb74ed
Anina Angelova <anina_1999@abv.bg>
2026-07-15 15:19:37 +0300

    Create user with plain password

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,6 +1,13 @@
-export function create(userData) {
-    console.log('Creating user in repo:', userData);
-}
+import { prisma } from '../lib/prisma.js';
+
+export async function create(userData) {
+    const result = await prisma.user.create({
+        data: {
+            email: userData.email,
+            password: userData.password,   
+        }
+    });
+};
 
 const userRepository = {
     create,
