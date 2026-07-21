93faf3adc5cae6a11e8149620c42b3fec33615e7
Anina Angelova <anina_1999@abv.bg>
2026-07-21 16:22:54 +0300

    Show dynamic navigation

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -10,6 +10,7 @@ export function authMiddleware(req, res, next) {
     try {
         const decodedToken = jwt.verify(token, 'SECRET');
         req.user = decodedToken;
+        res.locals.user = decodedToken;
     } catch (err) { 
         res.clearCookie('auth');
         res.redirect('/auth/login'); 
